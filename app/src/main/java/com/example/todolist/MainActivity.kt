9a01202f3a2614f6e512582599c189204eb3e7a1
David Whalley <77077742+dwhalley15@users.noreply.github.com>
2023-03-25 12:19:51 +0000

    Fragment created. Passes data from fragment to main activity.

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskBtn.setOnClickListener{
+            NewTaskSheet().show(supportFragmentManager, "newTaskTag")
+        }
+
+        taskViewModel.name.observe(this){
+            binding.taskName.text = String.format("Task Name: %s", it)
+        }
+
+        taskViewModel.desc.observe(this){
+            binding.taskDesc.text = String.format("Task Description: %s", it)
+        }
     }
 }
